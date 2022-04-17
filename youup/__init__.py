3c1d5164cf44bcf0789ec9f4eca2bd23b7d4fce5
Kadir Arslan <rslnkdr@gmail.com>
2022-04-18 00:54:02 +0300

    first deploy

--- youup/__init__.py
+++ youup/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-ENV = 'dev'
+ENV = 'prod'
 
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 if ENV == 'dev':
@@ -14,7 +14,7 @@ if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/youup'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wargdppuvuwirb:71fc54c56d982f7dc6cf873b278ec3c1f37a51369a77f5078b1be8835fb5a73b@ec2-23-20-224-166.compute-1.amazonaws.com:5432/dk7kk037ggkf6'
 db = SQLAlchemy(app)
 CORS(app)
 cors = CORS(app, resource={
